a8069441e1eae853e22c86c2d7fa9be985ac4546
RafaelBochi <rafaelbochibarros@gmail.com>
2023-06-25 23:00:36 -0300

    Created User

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'usuario',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -83,9 +85,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -101,3 +103,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "usuario.Usuario"
---
+++ usuario/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+from .managers import CustomUserManager
+
+
+class Usuario(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(_("e-mail address"), unique=True)
+    cpf = models.CharField(_("CPF"), max_length=11, blank=True, null=True)
+    telefone = models.CharField(_("Phone"), max_length=11, blank=True, null=True)
+    data_nascimento = models.DateField(
+        _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
+    )
+
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    EMAIL_FIELD = "email"
+
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        ordering = ["-date_joined"]
